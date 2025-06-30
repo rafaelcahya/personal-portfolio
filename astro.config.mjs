b1e4cb881d256d066c9bb875907ef90157dc7067
rafael cahya <cahyaputraugira95@gmail.com>
2025-06-30 14:28:17 +0700

    Update folder management and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,18 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://rafaelcahya.space', 
-    integrations: [react(), sitemap()],
+    site: 'https://rafaelcahya.space',
+    integrations: [react(),
+    sitemap(
+        {
+            filter: (page) =>
+                !page.includes("/offline/") &&
+                !page.includes("/404") &&
+                !page.includes("/400") &&
+                !page.includes("/error"),
+        }
+    )
+    ],
 
     vite: {
         plugins: [tailwindcss()],
